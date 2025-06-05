10d8ca8c8092d52598d734cd7c6bdbdfae1530e2
Thao Lê Thạc <thaolethac2121@gmail.com>
2025-06-06 03:04:01 +0700

    fix danhmuc

--- database/seeders/DanhMucSeeder.php
+++ database/seeders/DanhMucSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\DanhMuc;
+use App\Models\Danhmuc;
 
 class DanhMucSeeder extends Seeder
 {
@@ -17,7 +17,7 @@ class DanhMucSeeder extends Seeder
         ];
 
         foreach ($data as $item) {
-            DanhMuc::create($item);
+            Danhmuc::create($item);
         }
     }
 }
