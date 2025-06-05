f3c5b82ce837f5b1733802fcda44bb9b50d7bfd9
Thao Lê Thạc <thaolethac2121@gmail.com>
2025-06-06 02:15:00 +0700

    cc

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý cửa hàng túi xách</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('frontend/img/icon.png')}}" />
+    <link rel="shortcut icon" type="image/png" href="/frontend/img/icon.png" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link rel="stylesheet" href="{{ asset('frontend/css/bsgrid.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.min.css') }}" />
+    <link rel="stylesheet" href="/frontend/css/bsgrid.min.css" />
+    <link rel="stylesheet" href="/frontend/css/style.min.css" />
     <!-- <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}"> -->
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
@@ -237,7 +237,7 @@
         });
     </script>
 
-    <script src="{{ asset('frontend/script/script.js') }}"></script>
+    <script src="/frontend/script/script.js"></script>
 
 </body>
 
