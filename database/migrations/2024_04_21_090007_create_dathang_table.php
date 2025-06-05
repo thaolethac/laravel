875a1c49739a2d4b38045a6a09788947c4211e94
Thao Lê Thạc <thaolethac2121@gmail.com>
2025-06-06 00:23:48 +0700

    aa

--- database/migrations/2024_04_21_090007_create_dathang_table.php
+++ database/migrations/2024_04_21_090007_create_dathang_table.php
@@ -17,12 +17,12 @@ return new class extends Migration
             $table->increments('id_dathang'); // Change to increments for auto-increment
             $table->dateTime('ngaydathang')->nullable()->useCurrent();
             $table->dateTime('ngaygiaohang')->nullable()->useCurrent();
-            $table->integer('tongtien',111);
+            $table->unsignedBigInteger('tongtien');
             $table->string('phuongthucthanhtoan', 10);
             $table->string('diachigiaohang', 100)->nullable();
             $table->string('trangthai', 100)->nullable();
             $table->string('hoten', 100)->nullable();
-            $table->integer('sdt', 11)->nullable();
+            $table->unsignedBigInteger('sdt')->nullable();
             $table->string('email', 100)->nullable();
             $table->integer('id_kh');
             $table->index('id_dathang'); // Add index to id_dathang
